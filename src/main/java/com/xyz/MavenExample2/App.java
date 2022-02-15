c11138fa86801143ef31f0253bb3a0ecfaa29583
MYNAME <atuarnyc@gmail.com>
2022-02-15 17:16:54 -0500

    App file/class edited with SPLIT finction

--- src/main/java/com/xyz/MavenExample2/App.java
+++ src/main/java/com/xyz/MavenExample2/App.java
@@ -8,6 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String []array;
+    	
+        String str1 = "Buffalo Bangla is the first Bengali Newspaper in Buffalo, NY. It's the main source of News in Bengal";
+        
+        array = str1.split("\\s");
+        for(String s:array) {
+        	System.out.println(s);
+        }
+        System.out.println(array.length);
+        
     }
 }
